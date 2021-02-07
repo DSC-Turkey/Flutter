a332df385d04103d9ea0f24520aa0ead4f00f8d4
Barış Can Çamsarı <67071867+brscncmsr@users.noreply.github.com>
2021-02-07 15:39:55 +0300

    Full

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,172 @@
 import 'package:flutter/material.dart';
-import 'package:give_hand_main/Addpage.dart';
-import 'package:give_hand_main/Helpme.dart';
-import 'package:give_hand_main/Needhelp.dart';
+import 'package:give_hand_main/Mainmenu.dart';
+import 'package:give_hand_main/signup.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Givehand',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-        ),
-        home: MainMenu());
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/Home': (BuildContext context) => new MyHomePage(),
+        '/signup': (BuildContext context) => new SignupPage(),
+      },
+      home: new MyHomePage(),
+    );
   }
 }
 
-class MainMenu extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _MainMenuState createState() => _MainMenuState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MainMenuState extends State<MainMenu> {
-  int _activeIndex = 0;
-  List<Widget> _pages;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _pages = [NeedHelp(), Addpage(), Helpme()];
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.red[200]),
-        elevation: 20.0,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Container(
-          alignment: Alignment.topCenter,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Give",
-                style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              Text(
-                "Hand",
-                style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.red[200]),
-              )
-            ],
-          ),
-        ),
-      ),
-      body: _pages[_activeIndex],
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(0.0),
+    return new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Selçuk Mert"),
-              accountEmail: Text("selm123@mailim.com"),
-              currentAccountPicture: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://cdn.pixabay.com/photo/2016/03/09/15/10/man-1246508_960_720.jpg"),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(50.0),
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('GiveHand',
+                        style: TextStyle(
+                            fontSize: 70.0, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'EMAIL',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red))),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'ŞİFRE',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red))),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 5.0),
+                    Container(
+                      alignment: Alignment(1.0, 0.0),
+                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                      child: InkWell(
+                        child: Text(
+                          'Şifreni mi unuttun?',
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.redAccent,
+                        color: Colors.red,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MainMenu()));
+                          },
+                          child: Center(
+                            child: Text(
+                              'Giriş Yap',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      height: 40.0,
+                      color: Colors.transparent,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                style: BorderStyle.solid,
+                                width: 1.0),
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                              child: ImageIcon(AssetImage('assets/a.jpg')),
+                            ),
+                            SizedBox(width: 10.0),
+                            Center(
+                              child: Text('Google hesabı ile giriş yap',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat')),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Hesabın yok mu?',
+                  style: TextStyle(fontFamily: 'Montserrat'),
                 ),
-              ),
-              decoration: BoxDecoration(color: Colors.red[400]),
-            ),
-            ListTile(
-              title: Text("Yardımlarım"),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Puanlar"),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("ayarlar"),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Çıkış Yap"),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: Text(
+                    'Kaydol',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                )
+              ],
+            )
           ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _activeIndex,
-        selectedItemColor: Colors.red[400],
-        unselectedItemColor: Colors.grey[600],
-        items: [
-          BottomNavigationBarItem(
-              // ignore: deprecated_member_use
-              icon: Icon(Icons.hail),
-              title: Text("Yardım Talepleri")),
-          BottomNavigationBarItem(
-              // ignore: deprecated_member_use
-              icon: Icon(
-                Icons.add_box,
-                size: 50,
-              ),
-              title: Text("Ekle")),
-          BottomNavigationBarItem(
-              // ignore: deprecated_member_use
-              icon: Icon(Icons.group),
-              title: Text("Bağışlar"))
-        ],
-        onTap: (int tiklananButonPozisyonNo) {
-          setState(() {
-            _activeIndex = tiklananButonPozisyonNo;
-          });
-        },
-      ),
-    );
+        ));
   }
 }
